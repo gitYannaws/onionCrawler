325c4e7e067ef6df80f8163dcb078a2d663919df
Kyle Swanson <almightyswan@gmail.com>
2023-01-01 20:44:10 -0600

    update

--- dataCleaning.py
+++ dataCleaning.py
@@ -1,23 +1,49 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_colwidth', -1)
 
-df = pd.read_csv('coinData.csv')
+# df1 = pd.read_csv('coinData.csv')
+df2 = pd.read_csv('coinData2.csv')
+df3 = pd.read_csv('coinData3.csv')
+df4 = pd.read_csv('coinData3.csv')
+
+frames = [df2, df3, df4]
+df = pd.concat(frames)
+
+
 #2332
-dfM = df.loc[((df['coin'] == 'monero')|(df['coin'] == 'xmr'))&(df['count'] != 0)]['count'].sum()
-dfB = df.loc[((df['coin'] == 'bitcoin')|(df['coin'] == 'btc'))&(df['count'] != 0)]['count'].sum()
-dfL = df.loc[((df['coin'] == 'litecoin')|(df['coin'] == 'ltc'))&(df['count'] != 0)]['count'].sum()
-dfE = df.loc[((df['coin'] == 'ethereum')|(df['coin'] == r'\beth'))&(df['count'] != 0)]['count'].sum()
-dfZ = df.loc[((df['coin'] == 'zcash')|(df['coin'] == 'zec'))&(df['count'] != 0)]['count'].sum()
+dfM = df.loc[((df['coin'] == 'monero')|(df['coin'] == 'xmr'))&(df['count'] != 0)]['count'].count()
+dfB = df.loc[((df['coin'] == 'bitcoin')|(df['coin'] == 'btc'))&(df['count'] != 0)]['count'].count()
+dfL = df.loc[((df['coin'] == 'litecoin')|(df['coin'] == 'ltc'))&(df['count'] != 0)]['count'].count()
+dfE = df.loc[((df['coin'] == 'ethereum')|(df['coin'] == r'\beth'))&(df['count'] != 0)]['count'].count()
+dfZ = df.loc[((df['coin'] == 'zcash')|(df['coin'] == 'zec'))&(df['count'] != 0)]['count'].count()
 
-# df = df.loc[((df['coin'] == 'zcash')|(df['coin'] == 'zec'))&(df['count'] != 0)]
+# df = df.loc[(df['coin'] == 'zc23ash')|(df['coin'] == r'\beth\b')]
+# df = df.loc[((df['coin'] == 'monero')|(df['coin'] == 'xmr'))&(df['count'] != 0)]['count']
+df = df.loc[df['coin'] == 'ltc']
+df = df.reset_index()
 
-df = df.loc[df['count'] >= 10]
+print(df[['URL', 'count']])
 
-print(df[['coin', 'URL']])
-# print(dfM)
-# print(dfB)
-# print(dfL)
-# print(dfE)
-# print(dfZ)
+
+print("")
+print(dfM)
+print(dfB)
+print(dfL)
+print(dfE)
+print(dfZ)
+
+# df.to_csv("totalCoinData", sep='\t')
+
+# data = [dfM, dfB, dfL, dfE, dfZ]
+# labels = ['XMR', 'BTC', 'LTC', 'ETH', 'ZEC']
+#
+# #define Seaborn color palette to use
+# colors = sns.color_palette('pastel')[0:5]
+#
+# #create pie chart
+# plt.pie(data, labels = labels, colors = colors, autopct='%.0f%%')
+# plt.show()
--- dataScrapping.py
+++ dataScrapping.py
@@ -4,38 +4,51 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 from time import sleep
-import re
 import pandas as pd
+import re
+from fake_user_agent import user_agent
+
+
+#'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_6; en-us) AppleWebKit/525.27.1 (KHTML, like Gecko) Version/3.1.2 Safari/525.20.1'
+
+headers = {'User-Agent': user_agent()}
 
 socks.set_default_proxy(socks.SOCKS5, "localhost", 9150)
 socket.socket = socks.socksocket
-
+## darknet updating by search terms on the onions sites website with graph and charts updating monthly etc.
 def getaddrinfo(*args):
     return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
 
 socket.getaddrinfo = getaddrinfo
 
-website = ["https://github.com/mounibkamhaz/DarkWeb", "https://github.com/darknet11/onionlink", "https://dark.fail/", 'http://torlinkv7cft5zhegrokjrxj2st4hcimgidaxdmcmdpcrnwfxrr2zxqd.onion/', 'http://wvqwxc2pv54vodexdspx45nto5brtuomdibcdc5xk22kjnjjseov6kid.onion/']
-test = ['http://ransomwr3tsydeii4q43vazm7wofla5ujdajquitomtd47cxjtfgwyyd.onion/','https://github.com/mounibkamhaz/DarkWeb', 'https://lukesmith.xyz/', 'freedom hosting','http://fhostingineiwjg6cppciac2bemu42nwsupvvisihnczinok362qfrqd.onion/', 'https://cakewallet.com/', 'https://www.erowid.org/donations/donations_cryptocurrency.php', 'http://darkfailenbsdla5mal2mxn2uz66od5vtzd5qozslagrfzachha3f3id.onion/', 'http://hbwikiwsue3y74k6jyijzfdvc5h3lcgre3zwbjvghm4qngoj5wtuoeid.onion/', 'http://olinkspc6xf2poogjgufztrly7rg347n2stmurhuqb7bjqgevsnyygid.onion/', 'http://p23gulsmmneshdxabrbt3j2enmf2h4suynuujimjqjlklo4pprx6flid.onion/', 'http://22tojepqmpah32fkeuurutki7o5bmb45uhmgzdg4l2tk34fkdafgt7id.onion/']
+website1 = ["http://torlinkv7cft5zhegrokjrxj2st4hcimgidaxdmcmdpcrnwfxrr2zxqd.onion/"]
+website2 = ["https://github.com/mounibkamhaz/DarkWeb", "https://github.com/darknet11/onionlink", "https://dark.fail/", 'http://torlinkv7cft5zhegrokjrxj2st4hcimgidaxdmcmdpcrnwfxrr2zxqd.onion/', 'http://wvqwxc2pv54vodexdspx45nto5brtuomdibcdc5xk22kjnjjseov6kid.onion/', 'http://nf2nip7s2235bzv4zw2vpndctw2bkz6qgzut6n7wx62sud5m27o5fwad.onion/']
+website = ['https://github.com/mounibkamhaz/DarkWeb', 'https://lukesmith.xyz/', 'https://cakewallet.com/', 'https://www.erowid.org/donations/donations_cryptocurrency.php']
 dataCollected = []
 noWorkingData = []
 cryptoList = []
 addressCollected = []
-keySearchTerms = [r'bitcoin', r'monero', r'litecoin', r'zcash', r'ethereum', r'tether', r'btc', r'xmr', r'\beth', r'ltc', r'\bzec']
+keySearchTerms = [r'bitcoin', r'monero', r'litecoin', r'zcash', r'ethereum', r'tether']
+keySearchTickers = [ r'\bbtc\b', r'\bxmr\b', r'\beth\b', r'\bltc\b', r'\bzec\b', r'\bUSDT\b']
+keyTerms = [{'coin': r'bitcoin', 'ticker': r'\bbtc\b'}, {'coin': r'monero', 'ticker': r'\bxmr\b'}, {'coin': r'ethereum', 'ticker': r'\beth\b'},  {'coin': r'zcash', 'ticker': r'\bzec\b'}, {'coin': r'tether', 'ticker': r'\USDT\b'}]
+
+
 regexList = [{'type': 'bitcoin', 'subtype': 'native segwit', 'regexAddress': r'bc1q[ac-hj-np-z02-9]{38}'},{'type': 'bitcoin', 'subtype': 'P2SH', 'regexAddress': r'\b3[a-km-zA-HJ-NP-Z1-9]{25,34}\b'}, {'type': 'bitcoin', 'subtype': r'legacy', 'regexAddress': r'[1][A-Z][a-zA-HJ-NP-Z0-9]{32}\b'}, {'type': 'monero', 'subtype': 'main', 'regexAddress': r'[4][0-9AB][1-9A-HJ-NP-Za-km-z]{93}'}, {'type': 'monero', 'subtype': 'subaddress' , 'regexAddress': r'[8][0-9AB][1-9A-HJ-NP-Za-km-z]{93}'}, {'type': 'ethereum', 'subtype': None, 'regexAddress': r'0x[a-fA-F0-9]{40}'}, {'type': 'litecoin', 'subtype': r'legacy', 'regexAddress': r'[L][a-km-zA-HJ-NP-Z1-9]{33}\b'}, {'type': 'litecoin', 'subtype': r'segwit', 'regexAddress': r'[M][a-km-zA-HJ-NP-Z1-9]{33}\b'}, {'type': 'zcash', 'subtype': r'transparent', 'regexAddress': r't1[a-km-zA-HJ-NP-Z1-9]{33}'}, {'type': 'zcash', 'subtype': r'shielded', 'regexAddress': r'z1[a-km-zA-HJ-NP-Z1-9]{33}'}]
 workingOnionSite = []
-
-#scrapy? regex all key words? crawl within site??
+# eth regex add : and \b
+#scrapy? regex all key words? crawl within site?? look for "donate" amd crawl that first..? somehow scrap dread once it up..? add website with dread/general onion sets ranked
 #bc1q[ac-hj-np-z02-9]{38}
 #bc(0([ac-hj-np-z02-9]{39}|[ac-hj-np-z02-9]{59})|1[ac-hj-np-z02-9]{8,87})
 
 for web in website:
     try:
-        res = requests.get(web, timeout=15)
+        res = requests.get(web, timeout=30, headers=headers, verify=True)
+        print("Header: ", res.request.headers, res.status_code)
         soup = BeautifulSoup(res.content, 'html.parser')
         for a in soup.find_all('a', href=True):
-            if a['href'].find('.onion') != -1 and a['href'].find('porn') == -1 and a['href'].find('teen') == -1 and a['href'].find('xxx') == -1 and a['href'].find('boy') == -1 and a['href'][0:2].find('cp') == -1:
-                dataCollected.append(a['href'])
+            if a['href'].find('.onion') != -1:
+                if a['href'].find('porn') == -1 and a['href'].find('teen') == -1 and a['href'].find('xxx') == -1 and a['href'].find('boy') == -1 and a['href'][0:2].find('cp') == -1:
+                    dataCollected.append(a['href'])
 
     except Exception as e:
         print(f'Error: {e}')
@@ -43,13 +56,15 @@ for web in website:
 print(len(dataCollected))
 dataCollected = list(set(dataCollected))
 print(len(dataCollected))
+dataCollected = dataCollected[882:1329] ##882:1329
+print(len(dataCollected))
+print(dataCollected)
 
-
-for enum, site in enumerate(dataCollected):
-    sleep(1)
+for enum, site in enumerate(website):
+    sleep(15)
     print(enum, site)
     try:
-        res = requests.get(site, timeout=30, verify=False)
+        res = requests.get(site, timeout=30, verify=True, headers=headers)
         if res.status_code == 200:
             workingOnionSite.append(site)
             soup = BeautifulSoup(res.content, 'html.parser')
@@ -61,10 +76,12 @@ for enum, site in enumerate(dataCollected):
                     if len(address) <= 150:
                        addressCollected.append({'address': address, 'website': site, 'type': regex['type'], 'subtype': regex['subtype']})
 
-            for searchTerm in keySearchTerms:
-                shortCryptoList = re.findall(searchTerm, txt.lower())
-                if len(shortCryptoList) != 0:
-                    cryptoList.append({'coin': searchTerm, 'count': len(shortCryptoList), 'URL': site})
+            for searchTerm in keyTerms:
+                shortCoinList = re.findall(searchTerm['coin'], txt.lower())
+                shortTickerList = re.findall(searchTerm['ticker'], txt.lower())
+
+                if len(shortCoinList) != 0 or len(shortTickerList) != 0:
+                    cryptoList.append({'coin': searchTerm['coin'], 'count': len(shortCoinList) + len(shortTickerList), 'URL': site})
 
 
             print(len(cryptoList), len(addressCollected))
@@ -77,16 +94,14 @@ print(len(workingOnionSite))
 
 
 df = pd.DataFrame(data={"sites": workingOnionSite})
-df.to_csv("onionSites.csv", sep=',',index=False)
+df.to_csv("onionSitesTest.csv", sep=',',index=False)
 
-
-
-with open('coinData.csv', 'w', newline='') as f:
+with open('coinDataTest.csv', 'w', newline='') as f:
     writer = csv.DictWriter(f, fieldnames=['coin', 'URL', 'count'])
     writer.writeheader()
     writer.writerows(cryptoList)
 
-with open('onionAddresses.csv', 'w', newline='') as f:
+with open('onionAddressesTest.csv', 'w', newline='') as f:
     writer = csv.DictWriter(f, fieldnames=['address', 'website', 'type', 'subtype'])
     writer.writeheader()
     writer.writerows(addressCollected)
